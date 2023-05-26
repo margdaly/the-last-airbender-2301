626f684e37e9fb647a7813ce0cf087c6f594aefb
margdaly <margaret.daly@gmail.com>
2023-05-26 11:24:08 -0600

    Add vcr and webmock to spec/rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,3 +61,11 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+# VCR.configure do |config|
+#   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+#   config.hook_into :webmock
+#   config.default_cassette_options = { allow_playback_repeats: true }
+#   config.configure_rspec_metadata!
+#   config.ignore_localhost = true
+# end
