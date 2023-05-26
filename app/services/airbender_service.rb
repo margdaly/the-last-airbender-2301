cfe7ba5b1b14dd9be3d1a88cd68f4e15ba50e170
margdaly <margaret.daly@gmail.com>
2023-05-26 15:00:44 -0600

    add airbender service

--- app/services/airbender_service.rb
+++ app/services/airbender_service.rb
@@ -1,17 +1,21 @@
 class AirbenderService
   def nation_members(search_nation)
     search_nation.gsub!('_', '+')
+    get_url("/api/v1/characters?affiliation=#{search_nation}")
+  end
 
-    # conn = Faraday.new(url: 'https://last-airbender-api.fly.dev')
-    response = conn.get("/api/v1/characters?affiliation=#{search_nation}")
-    
-    payload = JSON.parse(response.body, symbolize_names: true)
-    @members = payload.map { |member_data| Member.new(member_data) }
+  def limit_per_page(character_amount)
+    get_url("/api/v1/characters?perPage=#{character_amount}")
   end
 
   private
 
-  def conn
+  def conn 
     Faraday.new(url: 'https://last-airbender-api.fly.dev')
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
